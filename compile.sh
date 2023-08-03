94f368bf1cddb83ac4b91c6df08d0be2ab95e46a
Raphael Goutmann <raphaelgoutmann@gmail.com>
2023-08-03 20:07:44 +0200

    Modification LICENSE && mini modification du script de compilation

--- compile.sh
+++ compile.sh
@@ -13,6 +13,5 @@ OUT_FILE="main.cpp"
 #    printf '#include "%s"\n' ${filename} >> ${OUT_FILE}
 #done
 
-echo "Done."
-
 `g++ -w -o a.out main.cpp interface/LovyanGFX/lgfx/Fonts/efont/*.c interface/LovyanGFX/lgfx/Fonts/IPA/*.c interface/LovyanGFX/lgfx/utility/*.c interface/LovyanGFX/lgfx/v1/*.cpp interface/LovyanGFX/lgfx/v1/misc/*.cpp interface/LovyanGFX/lgfx/v1/panel/Panel_Device.cpp interface/LovyanGFX/lgfx/v1/platforms/sdl/*.cpp  -lm -lSDL2  -lSDL_image -Iinterface/LovyanGFX -Iinterface/LovyanGFX/lgfx/v1 -Iinterface/LovyanGFX/lgfx/Fonts/efont -Linterface/LovyanGFX/lgfx -DLGFX_SDL`
+echo "Done."
