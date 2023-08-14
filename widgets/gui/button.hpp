020eb6b3e2323fbf7230adbcda93292b254ddd62
paxorch <paxo.rch@gmail.com>
2023-08-14 19:06:07 +0200

    fix gui constructors

--- widgets/gui/button.hpp
+++ widgets/gui/button.hpp
@@ -8,6 +8,21 @@ class Button : public Label
     public:
     GUI_TYPE getType(){return BUTTON_TYPE;}
 
+    Button()
+    {
+        init(0, 0, 0, 0);
+        this->fontHeight = 20;
+        this->bold = true;
+        enabledBackground=true;
+        
+        setBackgroundColor(COLOR_PRIMARY);
+        setTextColor(COLOR_LIGHT);
+        setRadius(10);
+        setHorizontalAlignment(CENTER_ALIGNMENT);
+        setVerticalAlignment(CENTER_ALIGNMENT);
+        setBorderSize(0);
+    }
+
     Button(int16_t x, int16_t y, int16_t width, int16_t height, std::string text)
     {
         init(x, y, width, height);
@@ -23,6 +38,7 @@ class Button : public Label
         setVerticalAlignment(CENTER_ALIGNMENT);
         setBorderSize(0);
     }
+
     void ClickEffect()
     {
         oldBackgroundColor = backgroundColor;
