80f2805173d38a89720420948b6df77e4e87761c
Raphaël Goutmann <raphaelgoutmann@gmail.com>
2023-11-28 18:55:05 +0100

    Modification interface/home_button (non définitif)

--- src/interface/button.cpp
+++ src/interface/button.cpp
@@ -7,21 +7,30 @@
     #include <esp_task_wdt.h>
 #endif
 
+#include <stdint.h>
+#include "../tasks/tasks.hpp"
+
 namespace home_button
 {
     #define HOME_BUTTON_PIN 33 // 32 pour l'ancien modèle
+
+    #define RELEASED 0
+    #define PRESSED  1
+
+    uint8_t status = RELEASED;
 
     void init(void)
     {
         #ifdef ESP32
             pinMode(HOME_BUTTON_PIN, INPUT_PULLUP);
         #endif
+        setInterval(new Callback<void>(&update), 10);
     }
 
-    bool isPressed(void)
+    void update(void)
     {
         #ifdef ESP32
-            return (HIGH == digitalRead(HOME_BUTTON_PIN));
+            status = (digitalRead(HOME_BUTTON_PIN) == HIGH);
         #endif
 
         #if defined(__linux__) || defined(_WIN32) || defined(_WIN64) || defined(__APPLE__)
@@ -30,10 +39,13 @@ namespace home_button
                 avec sdl pour pouvoir utiliser le home button
                 sur l'émulateur
             */  
-            return false;
+            status = RELEASED;
         #endif
+    }
 
-        return false;
+    bool isPressed(void)
+    {
+        return status;
     }
 
 }
